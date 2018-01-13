17a4884f60dfec7a822a7a8ce2831624d7939242
storysj <sean.story@digitalreasoning.com>
2018-01-13 17:39:12 -0600

    added the walking distance decision

---
+++ shelterbot/handlers/walking_distance_decision.py
@@ -0,0 +1,22 @@
+import logging
+from rapidsms.contrib.handlers.handlers.base import BaseHandler
+from ..utils import terminal_dialog
+
+
+logger = logging.getLogger(__name__)
+
+
+class WalkingDistanceDecisionHandler(BaseHandler):
+    """
+    For when the user has been asked if they can walk to an
+    emergency shelter
+    """
+
+    @classmethod
+    def dispatch(cls, router, msg):
+        msg_txt = msg.text.lower()
+        if msg_txt == "yes" or msg_txt == "y":
+            terminal_dialog.goodluck(msg)
+        elif msg_txt == "no" or msg_txt == "n":
+            # TODO: list pickup locations
+            return False # FIXME
