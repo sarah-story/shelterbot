559fc29bda8661d3eaf6e84090862ea4a4ea67b7
storysj <sean.story@digitalreasoning.com>
2018-03-10 13:36:51 -0600

    added string representations for the models, and made 'notes' optional

--- shelters/models.py
+++ shelters/models.py
@@ -9,6 +9,9 @@ class Address(models.Model):
     state = models.CharField(max_length=50)
     zip = models.IntegerField()
 
+    def __str__(self):
+        return '%s %s\n%s, %s %s' % (self.number, self.street, self.city, self.state, self.zip)
+
 
 class Shelter(models.Model):
     name = models.CharField(max_length=50)
@@ -18,5 +21,8 @@ class Shelter(models.Model):
     allows_children = models.BooleanField()
     allows_couples = models.BooleanField()
     allows_pets = models.BooleanField()
-    notes = models.CharField(max_length=1000)
+    notes = models.CharField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return '%s (%s %s)' % (self.name, self.address.number, self.address.street)
 
